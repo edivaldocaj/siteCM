ce54b9a88a23beea4e59f74c3d2ca6b260f86995
edivaldocaj <edivaldocaj@hotmail.com>
2026-03-31 15:53:55 -0300

    fix: postcss.cjs, install deps, copy collections novo

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,15 +1,11 @@
 import type { CollectionConfig } from 'payload'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
 
 export const Media: CollectionConfig = {
   slug: 'media',
   upload: {
-    // RESOLVE O CAMINHO ABSOLUTO PARA A PASTA PUBLIC DO NEXT.JS
-    staticDir: path.resolve(dirname, '../../public/media'),
+    // CORREÇÃO: Usa o process.cwd() para apontar sempre para a raiz real do projeto
+    staticDir: path.resolve(process.cwd(), 'public/media'),
     mimeTypes: ['image/*', 'application/pdf'],
     imageSizes: [
       { name: 'thumbnail', width: 400, height: 300, position: 'centre' },
